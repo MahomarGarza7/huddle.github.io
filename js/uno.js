d84065e043a0d2bf1d9637b89c944535724af241
PittnoiR <omargarza394@gmail.com>
2023-12-09 14:50:23 -0600

    se agregan los archivos

---
+++ js/uno.js
@@ -0,0 +1,41 @@
+
+ /*
+
+ var lol = "moped "
+ let num = "mamon";
+ num = 2;
+let otro = 5 ;
+
+const total = num + otro;
+ console.log(total); 
+
+
+const val = 'hola hijos de "de puta"';
+ console.log(val)
+
+const usuarioconectado = false ;
+
+let popo = 1 > 3;
+console.log(popo)
+
+const arreglo = [ 'texto', true ,666, {propiedad :'valor'},[1,2,4 ,false]];
+console.log(arreglo);
+
+
+const info = {
+    nom : 'lool',
+    numero :222222,
+
+    casa :{
+        numoo:5555,
+        color : 'red',
+        tienewifi: true
+        },
+
+};
+console.log(info.casa.tienewifi);
+ */
+
+
+
+
